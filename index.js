77550af0d7c35f565a63982d344aeadf098eb382
tiepbeobro <litt34lurknam1311@outlook.com>
2024-05-10 22:27:10 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const difference = (a, b) => {
-  const s = new Set(b);
-  return a.filter((x) => !s.has(x));
-};
+function levelOrderBottom(root) {
+  if (!root) return [];
+  const result = [];
+  const queue = [root];
+  while (queue.length) {
+    const levelSize = queue.length;
+    const currentLevel = [];
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift();
+      currentLevel.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.unshift(currentLevel);
+  }
+  return result;
+}
