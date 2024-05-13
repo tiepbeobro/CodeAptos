70e4e4bb9be7be3a993c81c862288af48b4b2edc
tiepbeobro <litt34lurknam1311@outlook.com>
2024-05-14 06:03:54 +0700

    start project

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-function linearSearch(arr, target) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) return i;
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
   }
-  return -1;
+  return arr;
 }
