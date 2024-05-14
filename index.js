912f876eadb9ca88d3b0f4f91e10c551eb0ec75d
tiepbeobro <litt34lurknam1311@outlook.com>
2024-05-14 13:09:54 +0700

    update sdk task

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let key = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > key) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-    arr[j + 1] = key;
-  }
-  return arr;
+function largestNumber(nums) {
+  if (nums.every((num) => num === 0)) return "0";
+  return nums.sort((a, b) => `${b}${a}` - `${a}${b}`).join("");
 }
